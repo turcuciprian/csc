246c43cb20798f16048ce870a0a464882c78ec8b
ciprianturcu <ciprianturcu@10.0.55.55>
2014-08-08 16:14:47 +0300

    First Commit

---
+++ csc.php
@@ -0,0 +1,11 @@
+<?php
+
+class csc {
+//    class global array variable
+    private $csc_array;
+//    function that get's fired every time there is a class declaration
+    public function __construct() {
+        
+    }
+
+}
---
+++ inc/forms.php
@@ -0,0 +1,13 @@
+<?php
+
+//Generate Forms Class
+class csc_forms {
+
+//    class global array variable
+    private $csc_array;
+
+//    function that get's fired every time there is a class declaration
+    public function __construct() {
+        
+    }
+}
